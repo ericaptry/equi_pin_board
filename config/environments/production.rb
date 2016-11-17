bd23c45d382eab95cad5e0f44655ab5491f6df8d
ericaptry”git config --global user.email erica.patry05@gmail.com” <erica.patry05@gmail.com>
2016-11-17 14:20:50 +0000

    updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,9 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
+  
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
